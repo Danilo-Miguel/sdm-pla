3858ffa292868d848b1e9cda9f4453ff717b7913
Tiago Xavier <822148404@ulife.com.br>
2025-08-22 11:42:56 -0300

    Criando funcoes

---
+++ funcoes.js
@@ -0,0 +1,24 @@
+// Função declarativa
+function saudacao(nome) {
+    return "Olá, " + nome + "!";
+}
+
+console.log(saudacao("Felipe"));
+
+// Função anônima
+const saudacaoAnonima = function(nome) {
+    return a + b;
+};
+
+function simples(){
+    let a = 5;
+    let b = 10;
+    let c = a + b;
+    console.log("O  valor de c é: " + c);
+}
+
+function soma(a, b) {
+    return a + b;
+}
+
+console.log(soma(5, 10));
